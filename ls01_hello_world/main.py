9e29159b5419de4056c6a0bf057ef51a93723e5b
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2022-04-07 22:46:59 -0500

    Leccion 01. Se imprime mensaje por consola con funcion print.

---
+++ ls01_hello_world/main.py
@@ -0,0 +1,20 @@
+#-*- coding: utf-8 -*-
+
+"""Leccion 01. Imprimir Hello World!
+
+Se emplea la funcion print para imprimir un mensaje en consola. Recibe como argumento
+un 'string' que se imprime en pantalla.
+
+Internamente la función print recibe una serie de elementos separados con comas que internamente
+se almacenan en una lista. Se abunda sobre el tema mas adelante.
+
+Notas:
+- Aunque no es regla, la guia de estilo nos indica que los comentarios de documentación (como este)
+se escriben dentro de comillas dobles, mientras que los strings se especifican dentro de comillas
+simples.
+
+- La primer linea NO es obligatoria, aunque por 'costrumbre' los pondré. :v
+"""
+
+# se imprime "Hello World!"
+print('Hello World!')
